45c2a8d4107ca503f4135a2b94cdf1728ac421ff
theravel <tarasovdyu@gmail.com>
2015-03-29 01:23:16 +0300

    Add basic program languages

---
+++ database/migrations/2015_03_28_221552_insert_program_languages_1/up.sql
@@ -0,0 +1,11 @@
+INSERT INTO program_languages ("name", "position") VALUES
+('C', 1),
+('Java', 2),
+('Objective-C', 3),
+('C++', 4),
+('C#', 5),
+('PHP', 6),
+('JavaScript', 7),
+('Python', 8),
+('Ruby', 9),
+('SQL', 10);
